a503cc8e086eedb7041422e97f2de43fdf8027e1
Josh Dales <git@joshdales.dev>
2023-11-30 15:39:56 -0500

    Create handler for DICOM images

--- main.go
+++ main.go
@@ -7,9 +7,21 @@ import (
 
 func main() {
 	server := http.NewServeMux()
+	server.HandleFunc("/images/:id", handleDicomImage)
 
 	err := http.ListenAndServe(":3333", server)
 	if err != nil {
 		fmt.Printf("Error while opening the server: %v", err)
+	}
+}
+
+func handleDicomImage(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// TODO: Get an image
+	case http.MethodPost:
+		// TODO: Store a new image
+	default:
+		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
 	}
 }
