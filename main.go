7fded4bd96795ca14315c2cb8a946746002a08fd
Josh Dales <git@joshdales.dev>
2023-12-01 10:09:21 -0500

    Basically start this approach again

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
-	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 func main() {
 	router := chi.NewRouter()
 
-	router.Route("/patient/{patientId}", func(r chi.Router) {
-		r.Use(authedUserHasAccessToPatient)
+	router.Use(checkUserAuthentication)
 
-		// Upload a DICOM image
-		r.Post("/image", handleDicomUpload)
-		// Query DICOM header attribute
-		r.Get("/patient/:patientId/image/:imageId", handleDicomAttributeQuery)
-		// Return the converted image as a PNG
-		r.Get("/patient/:patientId/image/:imageId/png", handlePngImage)
-	})
+	// Upload a DICOM image
+	router.Post("/image", handleDicomImageUpload)
+	// Return the image
+	router.Get("/image/{imageId}", handlePngImage)
 
 	err := http.ListenAndServe(":3333", router)
 	if err != nil {
@@ -27,26 +25,50 @@ func main() {
 	}
 }
 
-// I'm not actually implementing this, just writing out what we should be checking.
-// I'm assuming that a different microservice should be responsible for these checks.
-func authedUserHasAccessToPatient(next http.Handler) http.Handler {
+// I'm not actually implementing this, I'm assuming that a different
+// microservice should be responsible for this.
+func checkUserAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Make sure that the user is authenticated
-		// Then get the patient ID from the url and check that the patient exists
-
-		// Check that the logged in user has access to the patient:
-		// - If they are a patient then they must either be that patient or their guardian
-		// - If they are a clinician then they should have access to that patient,
-		//   i.e if you are a clinician who is not involved with that patient's care then you
-		//   shouldn't have access to their medical records.
-
-		// If we pass all these checks then great we can complete the request.
+		// Call whatever service we need to make sure that you have access
+		// Store the authed user's id in context
+		ctx := context.WithValue(r.Context(), "userId", uuid.New())
+		r.WithContext(ctx)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func handleDicomUpload(w http.ResponseWriter, r *http.Request) {}
+// Again I'm assuming that this is going to be handled by a separate
+// microservice and I'm not actually going to implement it.
+func userHasAccessToPatient(authedUserId string, permittedRoles []string, patientId string) bool {
+	// Check that the user has access to the patient, and the role for that endpoint.
+	// - If they are a patient then they must either be that patient or their guardian
+	// - If they are a clinician then they should have access to that patient,
+	//   i.e if you are a clinician who is not involved with that patient's care then you
+	//   shouldn't have access to their medical records.
+	// If we pass the check then great we can complete the request.
+	return true
+}
 
-func handleDicomAttributeQuery(w http.ResponseWriter, r *http.Request) {}
+func handleDicomImageUpload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(512)
+	if err != nil {
+		http.Error(w, "Could not parse the form data", http.StatusBadRequest)
+	}
+
+	patientId := r.Form.Get("patientId")
+	userId := r.Context().Value("userId").(string)
+	allowedRoles := make([]string, 1)
+	allowedRoles[0] = "clinician"
+	if !userHasAccessToPatient(userId, allowedRoles, patientId) {
+		http.Error(w, "You do not have access to this", http.StatusForbidden)
+	}
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+}
 
 func handlePngImage(w http.ResponseWriter, r *http.Request) {}
