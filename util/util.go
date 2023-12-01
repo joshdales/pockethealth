9fa3c63e9d6aaa2366142e75e31952b839665e18
Josh Dales <git@joshdales.dev>
2023-12-01 14:52:33 -0500

    Move all the util stuff into the same file

--- util/util.go
+++ util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"image/png"
+	"mime/multipart"
 	"os"
 	"pockethealth/dicom/db"
 
@@ -10,6 +11,27 @@ import (
 	"github.com/suyashkumar/dicom"
 	"github.com/suyashkumar/dicom/pkg/tag"
 )
+
+// In an actual production env I'm sure this would upload to your Object Storage, but I'm just storing locally
+func UploadImage(file multipart.File, patientId string) (string, error) {
+	var fileData []byte
+	file.Read(fileData)
+
+	imageId := uuid.New()
+	filePath := fmt.Sprintf("images/%s.dcm", imageId)
+
+	err := os.Mkdir("images", 0750)
+	if err != nil && !os.IsExist(err) {
+		return "", fmt.Errorf("Couldn't create images folder: %e", err)
+	}
+
+	err = os.WriteFile(filePath, fileData, 0660)
+	if err != nil {
+		return "", fmt.Errorf("Could not save file: %e", err)
+	}
+
+	return imageId.String(), nil
+}
 
 // Convert the DICOM image to png and upload it (or just store it locally in this case)
 func ConvertDicomToPngAndUpload(dataset *dicom.Dataset, dicomImgId string, patientId string) error {
